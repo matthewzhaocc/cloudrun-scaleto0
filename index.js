9bcaa0eee251387bb3cff67d7700d33d0874352b
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-03-13 12:00:40 -0800

    crud

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const bodyParser = require("body-parser")
+// gcp firestore
+const admin = require("firebase-admin")
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+})
+// le database
+const db = admin.firestore()
+const docRef = db.collection(process.env.COLLECTION).doc(process.env.DOC)
 let app = express()
 // logging
 app.use(morgan("common"))
 app.use(cors())
+app.use(bodyParser.json( {} ))
 // index route return hello
 app.get("/", (req, res) => {
     res.setHeader("Content-Type", "application/json")
@@ -16,6 +26,25 @@ app.get("/health", (req, res) => {
     res.setHeader("Content-Type", "application/json")
     res.setHeader("Application-Status", "healthy")
     res.send(JSON.stringify({"status": "up"}))
+})
+// new spidertron
+app.post("/spidertron/new", (req, res) => {
+    let name = req.body.name
+    docRef.set({
+        name: name
+    })
+    res.send("success")
+})
+
+// all spidys
+app.get("/spidertron", (req, res) => {
+    let pl = []
+    const ss = docRef.get()
+    ss.forEach((doc) => {
+        pl.push(doc)
+    })
+    res.send(pl)
+    res.setHeader("Content-Type", "text/plain")
 })
 // start listening
 app.listen(process.env.PORT || 3000, function() {
